71dd6ed3d117a03139417200164f4684cc97cdcc
alex-lemos <75213249+alex-lemos@users.noreply.github.com>
2022-08-06 22:55:51 -0300

    Desktop ok

--- js/app.js
+++ js/app.js
@@ -1,8 +1,23 @@
 
-/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+/*======= CHANGE BACKGROUND HEADER =======*/ 
 function scrollHeader(){
     const nav = document.querySelector("header")
     // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
     if(this.scrollY >= 50) nav.classList.add('active-header'); else nav.classList.remove('active-header')
 }
 window.addEventListener('scroll', scrollHeader)
+
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 'auto',
+    spaceBetween: 40,
+    grabCursor: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        type: 'progressbar',
+    },
+    navigation: {
+        nextEl: '.button-next',
+        prevEl: '.button-prev',
+    },
+  });
